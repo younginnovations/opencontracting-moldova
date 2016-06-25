3ca00d2f98e6b377a0d605e273273b98e9222d16
bezunakarmi <bezunakarmi@gmail.com>
2016-06-25 22:10:06 +0545

    [MO-89] Modify columns of contracts CSV file

--- db_dump/createCSV/create_contracts_csv.js
+++ db_dump/createCSV/create_contracts_csv.js
@@ -1,37 +1,43 @@
 /* 
-To run this script, run the following command in the cli
-# mongo localhost:27017/etenders create_contracts_csv.js
-*/
+ To run this script, run the following command in the cli
+ # mongo localhost:27017/etenders create_contracts_csv.js
+ */
 var start = new Date().getTime();
-print("starting Execution")
-db.tmp_contracts_summary.remove({})
-db.ocds_release.find().forEach(function(release){
-	release.contracts.forEach(function(contracts){
+print("starting Execution for Goods and Services");
+db.tmp_contracts_summary.remove({});
+db.ocds_release.find().forEach(function (release) {
+    release.contracts.forEach(function (contracts) {
+        /*Contract Id,Title,Description,Status,Tender Id,Tender Title,Goods,Goods CPV,Contractor Id,contractor,Procuring Agency id,Procuring Agency,Contract Start Date,Contract End Date,Amount*/
+        var t = {
+            'id'                : contracts['id'],
+            'title'             : contracts['title'],
+            'description'       : contracts['description'],
+            'status'            : contracts['status'],
+            'tenderID'          : release['tender']['id'],
+            'tenderTitle'       : release['tender']['title'],
+            'procuringAgencyID' : release['buyer']['identifier']['id'],
+            'procuringAgency'   : release['buyer']['name'],
+            'contractStartDate' : contracts['dateSigned'],
+            'contractEndDate'   : contracts['period']['endDate'],
+            'amount'            : contracts['value']['amount']
+        };
+        var awardID     = contracts['awardID'];
+        var myCursor    = db.ocds_release.find({'awards.id': awardID}, {'awards.suppliers': 1, 'awards.items': 1});
+        var myDocument  = myCursor.hasNext() ? myCursor.next() : null;
 
-		var t = {
-            'id'         	: contracts['id'],
-            'title'         : contracts['title'],
-            'description'   : contracts['description'],
-            'status' 		: contracts['status'],
-            'startDate'		: contracts['dateSigned'],
-            'endDate' 		: contracts['period']['endDate'],
-            'amount'		: contracts['value']['amount']
-        }
-		var awardID = contracts['awardID'];
-		var myCursor = db.ocds_release.find({'awards.id':awardID},{'awards.suppliers' : 1, 'awards.items' : 1});
-		var myDocument = myCursor.hasNext() ? myCursor.next() : null;
-		
-		if(myDocument['awards'].length){
-			t.contractor = (myDocument['awards'][0]['suppliers'])?myDocument['awards'][0]['suppliers'][0]['name']:'-';
-			t.goods = (typeof myDocument['awards'][0]['items'][0] !== 'undefined')?myDocument['awards'][0]['items'][0]['classification']['description']:'-';	
-		}
+        if (myDocument['awards'].length) {
+            t.contractorID  = (myDocument['awards'][0]['suppliers']) ? myDocument['awards'][0]['suppliers'][0]['additionalIdentifiers'][0]['id'] : '-';
+            t.contractor    = (myDocument['awards'][0]['suppliers']) ? myDocument['awards'][0]['suppliers'][0]['name'] : '-';
+            t.goods         = (typeof myDocument['awards'][0]['items'][0] !== 'undefined') ? myDocument['awards'][0]['items'][0]['classification']['description'] : '-';
+            t.goodsCPV      = (typeof myDocument['awards'][0]['items'][0] !== 'undefined') ? myDocument['awards'][0]['items'][0]['classification']['scheme'] : '-';
+        }
 
-        db.tmp_contracts_summary.insert(t);   
-	});                 
-       
+        db.tmp_contracts_summary.insert(t);
+    });
+
 });
 var end = new Date().getTime();
 var time = end - start;
-print("Execution Time (seconds) ", time/1000);
+print("Execution Time (seconds) ", time / 1000);
 
-//mongoexport -d etenders -c tmp_contracts_summary --type=csv --fields id,title,description,status,goods,contractor,startDate,endDate,amount --out contracts_csv.csv
+//mongoexport -d etenders -c tmp_contracts_summary --type=csv --fields id,title,description,status,tenderID,tenderTitle,goods,goodsCPV,contractorID,contractor,procuringAgencyID,procuringAgency,contractStartDate,contractEndDate,amount --out /Users/owner/homesteadSites/moldocds/public/csv/contracts_csv.csv
