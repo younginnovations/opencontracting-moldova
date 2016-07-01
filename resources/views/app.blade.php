6b6953ad312063495073854846e700358aaaca5a
bezunakarmi <bezunakarmi@gmail.com>
2016-07-01 13:45:39 +0545

    [MO-92]Add Google Analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -271,10 +271,23 @@
         var route = window.location.origin + window.location.pathname;
         return window.location.replace(route + '?lang=' + lang);
     });
-//    var changeLang = function (lang) {
-//        var route = window.location.origin + window.location.pathname;
-//        return window.location.replace(route + '?lang=' + lang);
-//    }
+</script>
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-80010335-1', 'auto');
+    ga('send', 'pageview');
+
 </script>
 </body>
 
